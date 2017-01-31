ac97fb0f5422426b09523cd99b9695be53097b98
Davide Nastri <d.nastri@gmail.com>
2017-01-31 16:24:08 +0100

    feat: Added notification module import check

--- port_checker.py
+++ port_checker.py
@@ -6,10 +6,22 @@
 # https://github.com/ltpitt/python-simple-notifications
 
 import socket
-import notification
 import time
 
-max_error_count = 100
+try:
+    import notification
+except ImportError:
+    print "Warning:"
+    print "Notification module is missing, no notifications will be sent."
+    print
+    print "You can download python-simple-notifications here:"
+    print "https://github.com/ltpitt/python-simple-notifications.git"
+    print
+
+# Customize those variables to meet your needs
+max_error_limit = 100
+host = '127.0.0.1'
+port = 80
 
 def reset_error_counter():
     # Reset error counter so it won't flood you with notifications
@@ -27,26 +39,32 @@ def increase_error_count():
     file = open('ErrorCount.log', 'w')
     file.write(str(error_count + 1))
     file.close()
-    if error_count == max_error_count:
+    if error_count == max_error_limit:
         # Send email, pushover, slack or do any other fancy stuff
-        print ("Sending out notification")
+        ko_message = "Port " + str(port) + " is not listening"
+        print(ko_message)
+        try:
+            notification.send_pushover_notification(ko_message)
+        except NameError:
+            pass
         reset_error_counter()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.settimeout(2) 
 
 reset_error_counter()
-result = 'start'
 
+result = 'start'
 while(result != 'done'):
-    result = sock.connect_ex(('127.0.0.1',8123))
+    result = sock.connect_ex((host,port))
     if result == 0:
-        print ("Port is open")
-        notification.send_pushover_notification("Home Assistant is now Online")
+        ok_message = "Port " + str(port) + " is now listening"
+        print (ok_message)
+        try:
+            notification.send_pushover_notification(ok_message)
+        except NameError:
+            pass
         result = 'done'
-    elif result == 50:
-        print("Port is still not open after 50 tries")
-        notification.send_pushover_notification("Home Assistant is not started yet")
     else:
         print ("Port is not open")
         increase_error_count()
